6bb459ece4dbe5b1dbde85f5254f59746f210c41
Nikolai Varankine <nvaranki@tut.by>
2013-09-19 21:52:55 +0000

    db structure displayed in the  browser

--- src/com/varankin/brains/jfx/BrowserRenderer.java
+++ src/com/varankin/brains/jfx/BrowserRenderer.java
@@ -74,27 +74,28 @@ public class BrowserRenderer
     static
     {
         // Иконки базы данных
-        КАТАЛОГ.put( com.varankin.brains.db.Проект.class, "load.png" );
-        КАТАЛОГ.put( com.varankin.brains.db.Модуль.class, "module.png" ); //TODO
+        КАТАЛОГ.put( com.varankin.brains.db.Проект.class, "new-project.png" );
+        КАТАЛОГ.put( com.varankin.brains.db.Модуль.class, "module.png" );
         КАТАЛОГ.put( com.varankin.brains.db.Расчет.class, "function.png" );
-        КАТАЛОГ.put( com.varankin.brains.db.Процессор.class, "processor2.png" );
+        КАТАЛОГ.put( com.varankin.brains.db.Процессор.class, "load1.png" );
         КАТАЛОГ.put( com.varankin.brains.db.Поле.class, "field2.png" );
-        КАТАЛОГ.put( com.varankin.brains.db.Сигнал.class, "signal.png" ); //TODO
-        КАТАЛОГ.put( com.varankin.brains.db.Точка.class, "point.png" ); //TODO
-        КАТАЛОГ.put( com.varankin.brains.db.Контакт.class, "pin.png" ); //TODO
-        КАТАЛОГ.put( com.varankin.brains.db.Соединение.class, "connector.png" ); //TODO
+        КАТАЛОГ.put( com.varankin.brains.db.Сигнал.class, "signal.png" );
+        КАТАЛОГ.put( com.varankin.brains.db.Точка.class, "point.png" );
+        КАТАЛОГ.put( com.varankin.brains.db.Контакт.class, "pin.png" );
+        КАТАЛОГ.put( com.varankin.brains.db.Соединение.class, "connector.png" );
+        КАТАЛОГ.put( com.varankin.brains.db.Фрагмент.class, "fragment.png" );
         // Иконки элементов мыслительной структуры        
         КАТАЛОГ.put( com.varankin.brains.artificial.Аргумент.class, "point.png" );
         КАТАЛОГ.put( com.varankin.brains.artificial.Ветвь.class, "point.png" );
         КАТАЛОГ.put( com.varankin.brains.artificial.Значение.class, "point.png" );
         КАТАЛОГ.put( com.varankin.brains.artificial.Источник.class, "transmitter2.png" );
         КАТАЛОГ.put( com.varankin.brains.artificial.Приемник.class, "receiver2.png" );
-        КАТАЛОГ.put( com.varankin.brains.artificial.Проект.class, "load.png" );
-        КАТАЛОГ.put( com.varankin.brains.artificial.ПроцессорРасчета.class, "processor2.png" );
-        КАТАЛОГ.put( com.varankin.brains.artificial.Разветвитель.class, "splitter.png" ); //TODO
+        КАТАЛОГ.put( com.varankin.brains.artificial.Проект.class, "new-project.png" );
+        КАТАЛОГ.put( com.varankin.brains.artificial.ПроцессорРасчета.class, "load1.png" );
+        КАТАЛОГ.put( com.varankin.brains.artificial.Разветвитель.class, "pin.png" );
         КАТАЛОГ.put( com.varankin.brains.artificial.Сенсор.class, "sensor.png" );
         КАТАЛОГ.put( com.varankin.brains.artificial.СенсорноеПоле.class, "field2.png" );
-        КАТАЛОГ.put( com.varankin.brains.artificial.Сигнал.class, "signal.png" ); //TODO
+        КАТАЛОГ.put( com.varankin.brains.artificial.Сигнал.class, "signal.png" );
     }
 
 }
