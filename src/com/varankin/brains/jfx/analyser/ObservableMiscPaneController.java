a497c3471088e7fdf6e743e6211dd84e7f48cd70
Nikolai Varankine <nvaranki@tut.by>
2014-04-26 21:57:38 +0000

    analyzer in progress - new observable setup

--- src/com/varankin/brains/jfx/analyser/ObservableMiscPaneController.java
+++ src/com/varankin/brains/jfx/analyser/ObservableMiscPaneController.java
@@ -1,11 +1,17 @@
 package com.varankin.brains.jfx.analyser;
 
+import com.varankin.brains.jfx.IntegerConverter;
+import com.varankin.brains.jfx.PositiveIntegerConverter;
 import com.varankin.property.PropertyMonitor;
 import com.varankin.util.LoggerX;
+import java.util.concurrent.Callable;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Builder;
@@ -21,22 +27,19 @@ public final class ObservableMiscPaneController implements Builder<Pane>
     private static final String RESOURCE_CSS  = "/fxml/analyser/ObservableMiscPane.css";
     private static final String CSS_CLASS = "observable-misc-pane";
 
-    private final ReadOnlyStringWrapper titleProperty;
-    private final ReadOnlyIntegerWrapper bufferProperty;
+    private final ReadOnlyObjectWrapper<Integer> bufferProperty;
     private final ReadOnlyBooleanWrapper validProperty;
 
     //TODO DEBUG START
     @Deprecated int count = 0;
     //TODO DEBUG END
 
+    @FXML private TextField title;
+    @FXML private TextField buffer;
+    
     public ObservableMiscPaneController()
     {
-        titleProperty = new ReadOnlyStringWrapper(
-        //TODO DEBUG START
-                "DEBUG"
-        //TODO DEBUG END
-        );
-        bufferProperty = new ReadOnlyIntegerWrapper(
+        bufferProperty = new ReadOnlyObjectWrapper<>(
         //TODO DEBUG START
                 1000
         //TODO DEBUG END
@@ -53,8 +56,29 @@ public final class ObservableMiscPaneController implements Builder<Pane>
     @Override
     public Pane build()
     {
+        title = new TextField(
+        //TODO DEBUG START
+                "DEBUG"
+        //TODO DEBUG END
+        );
+        title.setId( "title" );
+        title.setFocusTraversable( true );
+        title.setPrefColumnCount( 25 );
+        
+        buffer = new TextField(
+        //TODO DEBUG START
+                "1000"
+        //TODO DEBUG END
+        );
+        buffer.setId( "buffer" );
+        buffer.setFocusTraversable( true );
+        buffer.setPrefColumnCount( 6 );
         
         GridPane pane = new GridPane();
+        pane.add( new Label( LOGGER.text( "observable.setup.value.name" ) ), 0, 0 );
+        pane.add( title, 1, 0 );
+        pane.add( new Label( LOGGER.text( "observable.setup.buffer.name" ) ), 0, 1 );
+        pane.add( buffer, 1, 1 );
         
         pane.getStyleClass().add( CSS_CLASS );
         pane.getStylesheets().add( getClass().getResource( RESOURCE_CSS ).toExternalForm() );
@@ -67,19 +91,27 @@ public final class ObservableMiscPaneController implements Builder<Pane>
     @FXML
     protected void initialize()
     {
+        Bindings.bindBidirectional( buffer.textProperty(), bufferProperty, 
+                new IntegerConverter( buffer ) );
+        IntegerBinding bpb = Bindings.createIntegerBinding( () ->
+            {
+                Integer value = bufferProperty.getValue();
+                return value != null ? value : Integer.MIN_VALUE;
+            }, 
+            bufferProperty );
         BooleanBinding validBinding = 
             Bindings.and( 
-                Bindings.greaterThanOrEqual( bufferProperty, 0 ), 
-                Bindings.isNotEmpty( titleProperty ) );
+                Bindings.greaterThanOrEqual( bpb, 0 ), 
+                Bindings.isNotEmpty( title.textProperty() ) );
         validProperty.bind( validBinding );
     }
     
     ReadOnlyProperty<String> titleProperty()
     {
-        return titleProperty.getReadOnlyProperty();
+        return title.textProperty();
     }
 
-    ReadOnlyProperty<Number> bufferProperty()
+    ReadOnlyProperty<Integer> bufferProperty()
     {
         return bufferProperty.getReadOnlyProperty();
     }
@@ -98,7 +130,7 @@ public final class ObservableMiscPaneController implements Builder<Pane>
     {
 //        monitor = value;
         //TODO DEBUG START
-        titleProperty.setValue( value.getClass().getSimpleName() + count++ );
+        title.textProperty().setValue( value.getClass().getSimpleName() + count++ );
         //TODO DEBUG END
     }
 
