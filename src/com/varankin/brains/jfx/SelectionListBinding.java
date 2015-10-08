82874281d3d6f02ed33809b900b166b4429fe68f
Nikolai Varankine <nvaranki@tut.by>
2015-10-08 13:26:57 +0000

    one to one trnsforming ListBinding

--- src/com/varankin/brains/jfx/SelectionListBinding.java
+++ src/com/varankin/brains/jfx/SelectionListBinding.java
@@ -1,10 +1,5 @@
 package com.varankin.brains.jfx;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javafx.beans.binding.ListBinding;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.TreeItem;
@@ -13,43 +8,25 @@ import javafx.scene.control.TreeItem;
  * Список выбранных {@linkplain TreeItem элементов} {@linkplain TreeView дерева}.
  * 
  * @param <E> тип элемента.
+ * 
+ * @author &copy; 2015 Николай Варанкин
  */
-public class SelectionListBinding<E> extends ListBinding<E> 
+public class SelectionListBinding<E> extends OneToOneListBinding<TreeItem<E>,E> 
 {
-    private final ObservableList<E> LIST;
-    private final MultipleSelectionModel<TreeItem<E>> MODEL;
-
+    @Deprecated
     public SelectionListBinding( MultipleSelectionModel<TreeItem<E>> model ) 
     {
-        LIST = FXCollections.<E>observableArrayList();
-        MODEL = model;
-        super.bind( model.getSelectedItems() );
+        this( model.getSelectedItems() );
     }
 
-//    /**
-//     * Связывает данный список со списком выбора в {@link TreeView}.
-//     *
-//     * @param list список выбора в {@link TreeView}.
-//     */
-//    public void bind( ObservableList<TreeItem<E>> list ) 
-//    {
-//        super.bind( list );
-//    }
-
-    @Override
-    protected ObservableList<E> computeValue() 
+    public SelectionListBinding( ObservableList<TreeItem<E>> selection ) 
     {
-        List<E> value = //tree == null ? Collections.emptyList() :
-                getSelectedItems().stream()
-                .flatMap( (TreeItem<E> i) -> Stream.of( i.getValue() ) )
-                .collect( Collectors.toList() );
-        LIST.setAll( value );
-        return LIST;
+        super( selection, (TreeItem<E> i) -> i.getValue() );
     }
 
     public ObservableList<TreeItem<E>> getSelectedItems() 
     {
-        return MODEL.getSelectedItems();
+        return getSource();
     }
     
 }
